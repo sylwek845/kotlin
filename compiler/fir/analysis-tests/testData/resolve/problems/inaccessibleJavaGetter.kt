ffdce2313b7dd2079b178987f7292364bcc4db7b
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-09-24 12:01:04 +0300

    [FIR TEST] Add problematic test with should-be-inaccessible Java getter
    See KT-42116 for details

---
+++ compiler/fir/analysis-tests/testData/resolve/problems/inaccessibleJavaGetter.kt
@@ -0,0 +1,21 @@
+// FILE: PropertyDescriptor.java
+
+public interface PropertyDescriptor extends DescriptorWithAccessor {
+    String getSetter();
+}
+
+// FILE: test.kt
+
+interface DescriptorWithAccessor {
+    val setter: String
+}
+
+class WrappedPropertyDescriptor : PropertyDescriptor {
+    override val setter: String get() = "K"
+}
+
+fun test() {
+    val descriptor = WrappedPropertyDescriptor()
+    val res1 = descriptor.setter
+    val res2 = descriptor.getSetter() // Should be error
+}
